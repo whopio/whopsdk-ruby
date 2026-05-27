a8590ff7569656a1c09b6324171411155c9e512b
Daniel Schwartz <daniel@whop.com>
2026-05-27 16:06:24 -0700

    Build SDK
    Stainless-Generated-From: 85ec933d29b3952da74dafbc0ee7fe3571b8d54b

--- test/whop_sdk/client_test.rb
+++ test/whop_sdk/client_test.rb
@@ -35,7 +35,7 @@ class WhopSDKTest < Minitest::Test
   end
 
   def test_client_default_request_default_retry_attempts
-    stub_request(:get, "http://localhost/payments").with(query: hash_including({})).to_return_json(status: 500, body: {})
+    stub_request(:get, "http://localhost/payments").to_return_json(status: 500, body: {})
 
     whop = WhopSDK::Client.new(base_url: "http://localhost", api_key: "My API Key")
 
@@ -47,7 +47,7 @@ class WhopSDKTest < Minitest::Test
   end
 
   def test_client_given_request_default_retry_attempts
-    stub_request(:get, "http://localhost/payments").with(query: hash_including({})).to_return_json(status: 500, body: {})
+    stub_request(:get, "http://localhost/payments").to_return_json(status: 500, body: {})
 
     whop = WhopSDK::Client.new(base_url: "http://localhost", api_key: "My API Key", max_retries: 3)
 
@@ -59,7 +59,7 @@ class WhopSDKTest < Minitest::Test
   end
 
   def test_client_default_request_given_retry_attempts
-    stub_request(:get, "http://localhost/payments").with(query: hash_including({})).to_return_json(status: 500, body: {})
+    stub_request(:get, "http://localhost/payments").to_return_json(status: 500, body: {})
 
     whop = WhopSDK::Client.new(base_url: "http://localhost", api_key: "My API Key")
 
@@ -71,7 +71,7 @@ class WhopSDKTest < Minitest::Test
   end
 
   def test_client_given_request_given_retry_attempts
-    stub_request(:get, "http://localhost/payments").with(query: hash_including({})).to_return_json(status: 500, body: {})
+    stub_request(:get, "http://localhost/payments").to_return_json(status: 500, body: {})
 
     whop = WhopSDK::Client.new(base_url: "http://localhost", api_key: "My API Key", max_retries: 3)
 
@@ -83,7 +83,7 @@ class WhopSDKTest < Minitest::Test
   end
 
   def test_client_retry_after_seconds
-    stub_request(:get, "http://localhost/payments").with(query: hash_including({})).to_return_json(
+    stub_request(:get, "http://localhost/payments").to_return_json(
       status: 500,
       headers: {"retry-after" => "1.3"},
       body: {}
@@ -102,7 +102,7 @@ class WhopSDKTest < Minitest::Test
   def test_client_retry_after_date
     time_now = Time.now
 
-    stub_request(:get, "http://localhost/payments").with(query: hash_including({})).to_return_json(
+    stub_request(:get, "http://localhost/payments").to_return_json(
       status: 500,
       headers: {"retry-after" => (time_now + 10).httpdate},
       body: {}
@@ -121,7 +121,7 @@ class WhopSDKTest < Minitest::Test
   end
 
   def test_client_retry_after_ms
-    stub_request(:get, "http://localhost/payments").with(query: hash_including({})).to_return_json(
+    stub_request(:get, "http://localhost/payments").to_return_json(
       status: 500,
       headers: {"retry-after-ms" => "1300"},
       body: {}
@@ -138,7 +138,7 @@ class WhopSDKTest < Minitest::Test
   end
 
   def test_retry_count_header
-    stub_request(:get, "http://localhost/payments").with(query: hash_including({})).to_return_json(status: 500, body: {})
+    stub_request(:get, "http://localhost/payments").to_return_json(status: 500, body: {})
 
     whop = WhopSDK::Client.new(base_url: "http://localhost", api_key: "My API Key")
 
@@ -152,7 +152,7 @@ class WhopSDKTest < Minitest::Test
   end
 
   def test_omit_retry_count_header
-    stub_request(:get, "http://localhost/payments").with(query: hash_including({})).to_return_json(status: 500, body: {})
+    stub_request(:get, "http://localhost/payments").to_return_json(status: 500, body: {})
 
     whop = WhopSDK::Client.new(base_url: "http://localhost", api_key: "My API Key")
 
@@ -166,7 +166,7 @@ class WhopSDKTest < Minitest::Test
   end
 
   def test_overwrite_retry_count_header
-    stub_request(:get, "http://localhost/payments").with(query: hash_including({})).to_return_json(status: 500, body: {})
+    stub_request(:get, "http://localhost/payments").to_return_json(status: 500, body: {})
 
     whop = WhopSDK::Client.new(base_url: "http://localhost", api_key: "My API Key")
 
@@ -178,7 +178,7 @@ class WhopSDKTest < Minitest::Test
   end
 
   def test_client_redirect_307
-    stub_request(:get, "http://localhost/payments").with(query: hash_including({})).to_return_json(
+    stub_request(:get, "http://localhost/payments").to_return_json(
       status: 307,
       headers: {"location" => "/redirected"},
       body: {}
@@ -207,7 +207,7 @@ class WhopSDKTest < Minitest::Test
   end
 
   def test_client_redirect_303
-    stub_request(:get, "http://localhost/payments").with(query: hash_including({})).to_return_json(
+    stub_request(:get, "http://localhost/payments").to_return_json(
       status: 303,
       headers: {"location" => "/redirected"},
       body: {}
@@ -231,7 +231,7 @@ class WhopSDKTest < Minitest::Test
   end
 
   def test_client_redirect_auth_keep_same_origin
-    stub_request(:get, "http://localhost/payments").with(query: hash_including({})).to_return_json(
+    stub_request(:get, "http://localhost/payments").to_return_json(
       status: 307,
       headers: {"location" => "/redirected"},
       body: {}
@@ -258,7 +258,7 @@ class WhopSDKTest < Minitest::Test
   end
 
   def test_client_redirect_auth_strip_cross_origin
-    stub_request(:get, "http://localhost/payments").with(query: hash_including({})).to_return_json(
+    stub_request(:get, "http://localhost/payments").to_return_json(
       status: 307,
       headers: {"location" => "https://example.com/redirected"},
       body: {}
@@ -281,7 +281,7 @@ class WhopSDKTest < Minitest::Test
   end
 
   def test_default_headers
-    stub_request(:get, "http://localhost/payments").with(query: hash_including({})).to_return_json(status: 200, body: {})
+    stub_request(:get, "http://localhost/payments").to_return_json(status: 200, body: {})
 
     whop = WhopSDK::Client.new(base_url: "http://localhost", api_key: "My API Key")
 
